faa8e9ce7bcfd0228e8d5813f04ad86ad48724f5
AyaMajid591 <ayariahi954@gmail.com>
2025-01-18 13:49:00 +0600

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,87 @@
+# name = 'Tom'
+# print (name)
+# name = 'sam'
+# print (name)
+# from pickletools import name2
+# from tkinter.font import names
+#
+# from libpasteurize.fixes.fix_imports import multiple_name_import_match
+
+# NAME = 'Aya'
+# print(NAME)
+# Name = 'Aya'
+# print('Name')
+
+# r = 10
+# pi = 3.14
+# a = 2*r*r*pi
+# print(a)
+
+# c = 6
+# f = c*9/5+32
+# print(f)
+
+# num1 = 10
+# num2 = 12
+# summa = num1+num2
+# print(summa)
+# delenie = num1/num2
+# print(delenie)
+# minus = num1-num2
+# print(minus)
+# times = num1*num2
+# print(times)
+
+# age = input('What is your age?')
+# print(age)
+
+# name = 'Mary'
+# age = 20
+# print("Her name is", name, "her age is", age)
+# print(f"Her name is {name} her age is {age}")
+
+
+# name
+# Name
+# Name
+# name1
+# name2
+# myNAME
+# my_name
+# name_
+# _name
+
+
+# num1 = 5
+# num2 = 2.5
+# num3 = int(num2)
+# print(num3)
+
+# num1 = int(input("insert your number"))
+# num2 = int(input("insert your number"))
+# suma = num1+num2
+# delenie = num1/num2
+# print(f"first number was {num1}, /nsecond number was {num2} /nsuma equals {suma} /nthe difference is equals {delenie}")
+
+
+# num3 = float(input("insert your number"))
+# num4 = float(input("insert your number"))
+# suma = num3+num4
+# delenie = num3/num4
+# print(f"first number {num3}, /n second number was {num4} /n suma equals {suma} the whole part{int(suma)} /n difference is equals{delenie}")
+
+# name = input("What's your name?")
+# age = input("What's your age?")
+# print(f"Your name is{name}, your age is{age}")
+
+# num1 = int(input("insert your number"))
+# num2 = int(input("insert your number"))
+# suma = num1+num2
+# print(f"first number was {num1}, /n second number was {num2} /n suma equals {suma}")
+
+
+
+
+
+
+
