7b3148d5996c85137cbc88ffb8a319bdaea3652f
AyaMajid591 <ayariahi954@gmail.com>
2025-01-18 13:44:11 +0600

    Add files via upload
    python ms alia

---
+++ homework.py
@@ -0,0 +1,50 @@
+# drink1 = int(input("I have drinks: "))
+# drink2 = int(input("A customer takes drinks: "))
+# minus= drink1 - drink2
+# price_per_drink = 3
+# total_cost = drink2 * price_per_drink
+# print(f"I started with {drink1} drinks.")
+# print(f"A customer took {drink2} drinks.")
+# print(f"I now have {minus} drinks left.")
+# print(f"Each drink costs ${price_per_drink}.")
+# print(f"The total cost for the customer is ${total_cost}.")
+# print("Thank you for your purchase!")
+
+# num1 = 10
+# num2 = 12
+# summa = num1+num2
+# print(summa)
+# delenie = num1/num2
+# print(delenie)
+# minus = num1-num2
+# print(minus)
+# times = num1*num2
+# print(times)
+
+# name = 'Mary'
+# age = 20
+# print("Her name is", name, "her age is", age)
+# print(f"Her name is {name} her age is {age}")
+
+
+# num1 = int(input("insert your number"))
+# num2 = int(input("insert your number"))
+# suma = num1+num2
+# delenie = num1/num2
+# print(f"first number was {num1}, /nsecond number was {num2} /nsuma equals {suma} /nthe difference is equals {delenie}")
+
+
+# num3 = float(input("insert your number"))
+# num4 = float(input("insert your number"))
+# suma = num3+num4
+# delenie = num3/num4
+# print(f"first number {num3}, /n second number was {num4} /n suma equals {suma} the whole part{int(suma)} /n difference is equals{delenie}")
+
+# name = input("What's your name?")
+# age = input("What's your age?")
+# print(f"Your name is{name}, your age is{age}")
+
+# num1 = int(input("insert your number"))
+# num2 = int(input("insert your number"))
+# suma = num1+num2
+# print(f"first number was {num1}, /n second number was {num2} /n suma equals {suma}")
