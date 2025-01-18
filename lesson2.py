04176aed917bff7c694bbe5a4aae10f103dc936a
AyaMajid591 <ayariahi954@gmail.com>
2025-01-18 13:49:24 +0600

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,41 @@
+# num1 = int(input("insert your number"))
+# num2 = int(input("insert your number"))
+# suma = num1+num2
+# delenie = num1/num2
+# print(f"first number was {num1}, /nsecond number was {num2} /nsuma equals {suma} /nthe difference is equals {delenie}")
+
+
+# num3 = float(input("insert your number"))
+# num4 = float(input("insert your number"))
+# suma = num3+num4
+# delenie = num3/num4
+# print(f"first number {num3}, /n second number was {num4} /n suma equals {suma} the whole part{int(suma)} /n difference is equals{delenie}")
+
+# name = input("What's your name?")
+# age = input("What's your age?")
+# print(f"Your name is{name}, your age is{age}")
+
+# num1 = int(input("insert your number"))
+# num2 = int(input("insert your number"))
+# suma = num1+num2
+# print(f"first number was {num1}, /n second number was {num2} /n suma equals {suma}")
+
+# num1 = int(input("insert your number"))
+# num2 = int(input("insert your number"))
+# num3 = int(input("insert your number"))
+# suma = num1+num2+num3
+# print(f"first number was {num1}, \nsecond number was {num2},\nthird number was {num3} \nsuma equals {suma}")
+
+# price = 12
+# print(type(price))
+# price = price + 4.5
+
+# name = "sam"
+# print(type(name))
+
+# drink1 = int(input("I have drinks"))
+# drink2 = int(input("A customer takes drinks"))
+# minus = drink1 - drink2
+# print(f'i have {drink1}, \ncustomer takes {drink2}  \ni was left with {minus} \nand my customer gave me a tip {int(minus)}, ') practise work.
+
+
